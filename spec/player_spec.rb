2344d66033f32e228d527b3863af304bc8f3a529
bagool185 <ioan.adrian98@gmail.com>
2017-09-04 19:21:00 +0300

    first commit

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,5 +4,20 @@ RSpec.describe Player do
   it "has a name assigned through constructor" do 
 		player = Player.new("bagool")
 		expect(player.name).to eql "bagool"
-  end 
+	end 
+	
+	it "has a color" do 
+		player = Player.new("bagool")
+		expect(player.name).not_to be nil
+	end 
+
+	it "has white pieces by default" do 
+		player = Player.new("bagool")
+		expect(player.color).to eql "white"
+	end 
+
+	it "has black pieces if specified" do 
+		player = Player.new("bagool", "black")
+		expect(player.color).to eql "black"
+	end 
 end
