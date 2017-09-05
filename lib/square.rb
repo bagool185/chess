33a445c3b55900e5a214dfdbf4d803d110580dfc
bagool185 <ioan.adrian98@gmail.com>
2017-09-05 11:58:11 +0300

    deleted square class

--- lib/square.rb
+++
@@ -1,9 +0,0 @@
-class Square 
-	attr_accessor :color, :X, :Y 
-		
-	def initialize(x_coordinate, y_coordinate, color = "white")
-		@X = x_coordinate 
-		@Y = y_coordinate
-		@color = color 
-	end
-end 
