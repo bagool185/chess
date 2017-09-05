56182290a540379f12a41f60c4ca1551f0680ba5
bagool185 <ioan.adrian98@gmail.com>
2017-09-05 12:22:34 +0300

    added piece property to square

--- lib/square.rb
+++ lib/square.rb
@@ -1,9 +1,12 @@
 class Square 
-	attr_accessor :color, :X, :Y 
+	attr_accessor :color, :X, :Y, :label, :adjacency_list, :piece
 		
-	def initialize(x_coordinate, y_coordinate, color = "white")
+	def initialize(x_coordinate, y_coordinate, label, color = "white")
 		@X = x_coordinate 
 		@Y = y_coordinate
 		@color = color 
+		@label = label
+		@adjacency_list = [] 
+		@piece = nil 
 	end
 end 
