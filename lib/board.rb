4cfa4802784aaf8a7275661148e5bcbb1faf6def
bagool185 <ioan.adrian98@gmail.com>
2017-09-05 13:51:40 +0300

    provided comments

--- lib/board.rb
+++ lib/board.rb
@@ -6,13 +6,14 @@ class Board
 
 	def initialize
 		@size = 8 
+		# squares will be a graph
+		# the method build_squares pushes the nodes into the graph
 		@squares = build_squares
 	end
 
 	def build_squares 
 		crt_color = "black"
-
-		temp_matrix = []
+		temp_graph = []
 
 		(1..8).reverse_each do |i| 
 			('A'..'H').each do |j| 	
@@ -33,12 +34,12 @@ class Board
 				square = Square.new(i, j, label, crt_color)
 				square.piece = piece
 				# prepend the node into the graph
-				temp_matrix. << (square)
+				temp_graph. << (square)
 				crt_color = change_color(crt_color) unless j == 'H'
 			end
 		end 
 
-		return temp_matrix
+		return temp_graph
 	end
 
 	def change_color(crt_color) 
@@ -48,8 +49,13 @@ class Board
 
 	def display_board 
 		@squares.each_with_index do |square, index|
-			 puts "\n" if index % 8 == 0
+			# if the index is a multiple of 8, then the line has finished			
+			puts "\n" if index % 8 == 0
 
+			# if the square doesn't have any square on it, it will print 
+			# a white or black square depending on the position
+			# white square -> U+25A0
+			# black square -> U+25A1
 			if square.piece.nil? 
 				print "\u25A0 " if square.color == "black"
 				print "\u25A1 " if square.color == "white"	
