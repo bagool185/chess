cc270aa1a10eddb816eb5f030671c80f9e1901d2
bagool185 <ioan.adrian98@gmail.com>
2017-09-19 12:03:25 +0300

    finished movement class

--- lib/movement.rb
+++ lib/movement.rb
@@ -1,2 +1,12 @@
+require_relative "piece"
+
 class Movement 
-end 
+  attr_accessor :piece, :vertical, :horizontal, :diagonal 
+
+  def initialize(piece, vertical, horizontal, diagonal) 
+    @piece = piece 
+    @vertical = vertical
+    @horizontal = horizontal 
+    @diagonal = diagonal
+  end
+end
